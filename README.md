f02d93ca9c7369d22a2cfb87b3a0a01ff55d3696
Kelsi <kelsilane91@gmail.com>
2020-12-10 16:38:19 -0600

    exercise 1

---
+++ README.md
@@ -0,0 +1,10 @@
+# RN Starter App
+### From Udemy course with Stephen Grider
+
+
+
+# Set Up
+```
+clone project, cd into it, & npm install
+
+after scanning expo if you can't connect, try pointing to Tunnel on the client instead of LAN
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return <Text style={styles.text}>Welcome to the RN Course!</Text>;
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+    textAlign: "center",
+  },
 });
 
 export default HomeScreen;
