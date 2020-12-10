065bf633c3a0464cc2bda172e5733c836bab7654
Kelsi <kelsilane91@gmail.com>
2020-12-10 16:44:25 -0600

    initial code for list screen

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ListScreen = () => {
+  return (
+    <View>
+      <Text>List Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
