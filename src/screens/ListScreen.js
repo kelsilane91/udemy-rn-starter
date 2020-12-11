37d3db6fee9ed77816d16e3bcce1c7d2303e03b2
Kelsi <kelsilane91@gmail.com>
2020-12-10 18:13:46 -0600

    completed flatlist section

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,14 +1,65 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 const ListScreen = () => {
+  const friends = [
+    {
+      name: "Natasha",
+      age: 34,
+    },
+    {
+      name: "Steve",
+      age: 100,
+    },
+    {
+      name: "Tony",
+      age: 48,
+    },
+    {
+      name: "Thor",
+      age: 99999,
+    },
+    {
+      name: "Bruce",
+      age: 49,
+    },
+    {
+      name: "Clint",
+      age: 47,
+    },
+    {
+      name: "Carol",
+      age: 60,
+    },
+    {
+      name: "Peter",
+      age: 18,
+    },
+    {
+      name: "Stephen",
+      age: 42,
+    },
+  ];
   return (
-    <View>
-      <Text>List Screen</Text>
-    </View>
+    <FlatList
+      data={friends}
+      renderItem={({ item }) => {
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
+      }}
+      keyExtractor={(friend) => friend.name}
+    />
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+  },
+});
 
 export default ListScreen;
