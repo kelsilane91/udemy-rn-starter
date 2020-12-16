ed5b7de656f7e9e6e7515f72815e62d60e8e2d91
Kelsi <kelsilane91@gmail.com>
2020-12-16 09:15:12 -0600

    section 7 - intro to flexbox

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,23 +4,31 @@ import { StyleSheet, Text, View } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Box Screen</Text>
+      <Text style={styles.text1}>Child #1</Text>
+      <Text style={styles.text2}>Child #2</Text>
+      <Text style={styles.text3}>Child #3</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    borderWidth: 3,
     borderColor: "black",
-    backgroundColor: "blue",
+    borderWidth: 3,
+    height: 200,
   },
-  text: {
-    borderWidth: 10,
+  text1: {
     borderColor: "red",
-    margin: 20,
-    padding: 10,
-    backgroundColor: "yellow",
+    borderWidth: 3,
+  },
+  text2: {
+    borderColor: "red",
+    borderWidth: 3,
+    alignSelf: "flex-start",
+  },
+  text3: {
+    borderColor: "red",
+    borderWidth: 3,
   },
 });
 
